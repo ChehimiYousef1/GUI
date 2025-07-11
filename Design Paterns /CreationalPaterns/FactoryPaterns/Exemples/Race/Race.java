29fbce66626fe2fef92fec0da2ce66031b3f7b11
ChehimiYousef1 <122696157+ChehimiYousef1@users.noreply.github.com>
2025-07-11 19:26:00 +0300

    Update Race.java

--- Design Paterns /CreationalPaterns/FactoryPaterns/Exemples/Race/Race.java
+++ Design Paterns /CreationalPaterns/FactoryPaterns/Exemples/Race/Race.java
@@ -1,1 +1,10 @@
-
+abstract class Race{
+  List<Bicycle> Bikes;
+  public abtract Bicycles createRace(){
+    public void createRace(){
+      for(int i = 0;i < n;i++){
+        bikes.add(createBicycles());
+      }
+    }
+  }
+}
